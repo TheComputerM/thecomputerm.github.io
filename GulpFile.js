0d73432973df281a7f546aadb0ed1877cbba5c3a
TheComputerM <mudit.harshit99@outlook.com>
2019-11-01 20:56:17 +0530

    Started Project
    🥳🥳🥳

---
+++ GulpFile.js
@@ -0,0 +1,51 @@
+const { src, dest, watch, series, parallel } = require('gulp');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
+let browserSync = require('browser-sync').create();
+
+function js() {
+    return browserify('./src/js/main.js')
+        .transform('babelify', { presets: ['@babel/preset-env'] })
+        .transform('uglifyify')
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(dest('./dist/js/'));
+}
+
+function css() {
+    return src('./src/scss/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(autoprefixer())
+        .pipe(dest('./dist/css/'));
+}
+
+function betterial() {
+    return src('./src/betterial/sass/betterial.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(autoprefixer())
+        .pipe(dest('./dist/betterial/css/'));
+}
+
+function dev() {
+    browserSync.init({
+        server: {
+            baseDir: './',
+        },
+    });
+    watch('./src/scss/**/*.scss').on('change', series(css, browserSync.reload));
+    watch('./src/js/main.js').on('change', series(js, browserSync.reload));
+    watch('./index.html').on('change', browserSync.reload);
+}
+
+exports.js = js;
+exports.css = css;
+exports.betterial = betterial;
+exports.compile = parallel(css, js);
+exports.dev = dev;
