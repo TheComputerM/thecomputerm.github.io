a4111535ab0e4742aa3cac1b6dd8705d7a68ff1d
Mudit Somani <mudit.somani00@outlook.com>
2022-09-17 21:16:57 +0530

    add vanilla-extract

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import solidJs from "@astrojs/solid-js";
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+  integrations: [solidJs()],
+  vite: {
+    plugins: [vanillaExtractPlugin()]
+  }
 });
