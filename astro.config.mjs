a5ad6381f52e115431c27a6aa57aa55e067665f0
TheComputerM <mudit.somani00@gmail.com>
2025-01-21 03:54:46 +0530

    feat: add shiki theme for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,14 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  integrations: [solidJs()],
+	output: "server",
+	integrations: [solidJs()],
+	markdown: {
+		shikiConfig: {
+			themes: {
+				light: "github-light-high-contrast",
+				dark: "github-dark-high-contrast",
+			},
+		},
+	},
 });
