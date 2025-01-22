4def9936319e122f4d434d955e2b7b5e1af22a26
TheComputerM <mudit.somani00@gmail.com>
2025-01-22 22:43:52 +0530

    feat: static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
+	output: "static",
 	integrations: [solidJs()],
 
 	markdown: {
