e63f000d0b1b1d29f937f7354dfc663c28bbb70e
TheComputerM <mudit.somani00@gmail.com>
2025-01-22 22:31:01 +0530

    feat: remove markdown rendering

--- src/utils/render-markdown.ts
+++ src/utils/render-markdown.ts
@@ -1,15 +1,1 @@
-import chalk from "chalk";
-import { marked } from "marked";
-import { markedTerminal } from "marked-terminal";
-
-marked.use(
-	// @ts-ignore
-	markedTerminal({
-		href: chalk.red,
-		reflowText: true,
-	}),
-);
-
-export const renderMD = (input: string) => {
-	return marked(input) as string;
-};
+// TODO, maybe
